6aa970c98bf75035ed07bf29d52ae4de5c555351
Mario Buonomo <ma.buonomo@gmail.com>
2020-01-11 21:37:12 +0100

    clean

--- protobuf/compiler/compile.sh
+++ protobuf/compiler/compile.sh
@@ -6,6 +6,5 @@ OUT_DIR="./generated"
 
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="service=grpc-node:${OUT_DIR}" \
     ./protos/*.proto
